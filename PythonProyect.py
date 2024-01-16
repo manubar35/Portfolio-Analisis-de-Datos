70ca387c0a6cd36066a4a88ace6382325fb62a88
manubar35 <97482756+manubar35@users.noreply.github.com>
2024-01-16 15:08:45 -0300

    Update PythonProyect.py

--- PythonProyect.py
+++ PythonProyect.py
@@ -1,25 +1,17 @@
 import pandas as pd
-import numpy as np
+import openpyxl
+import matplotlib.pyplot as plt
+path = "C:\\Users\\Manjul\\Documents\\F\\apendice5.xlsx"
+df1 = pd.read_excel(path, sheet_name= "1. ICA")
+df = df1.drop(index=range(4))
+nuevos_encabezados = df.iloc[0]
+df = df[2:].rename(columns=nuevos_encabezados)
+df = df[["Fecha","Expo Totales","Impo Totales","Saldo Comercial"]]
+df = df.fillna(0)
+df = df[df["Fecha"] != 0]
+df['Variacion_Saldo'] = (df['Saldo Comercial'].pct_change() * 100)
+df['Variacion_Expo'] = (df['Expo Totales'].pct_change() * 100)
+df.round()
+df = df.tail(8)
+df.astype(int)
 
-calendario = pd.read_excel("Calendario.xlsx")
-CategoriaProducto = pd.read_excel("CategoriaProducto.xlsx")
-Descuentos = pd.read_excel("Descuentos.xlsx")
-Pedidos = pd.read_excel("Pedidos.xlsx")
-Producto = pd.read_excel("Producto.xlsx")
-
-#Consulta con condiciones
-print(Producto[(Producto["Categoría"] == "Libro") & (Producto["Tamaño de Empaque"] == "Mediano")])
-#Cantidad de producto por categoría
-print(Producto.groupby("Categoría").size())
-#Descuentos por orden de Mayor a Menor
-print(Descuentos["Descuento"].sort_values(ascending=False))
-#Actualizacion de datos
-CategoriaProducto.loc[CategoriaProducto["SKU"]=="B01", "Precio de Venta"] = 191
-print(CategoriaProducto.head(5))
-#Describe
-print(Pedidos.describe())
-#Joins
-CombinacionInner = pd.merge(CategoriaProducto,Producto)
-print(CombinacionInner[["SKU","Precio de Venta","Categoría"]][CombinacionInner["Categoría"]=="Blue"])
-CombinacionInner2 = pd.merge(CategoriaProducto,Producto,how="inner", on=["SKU"])
-print(CombinacionInner2[["SKU","Precio de Venta","Categoría"]][CombinacionInner2["Categoría"]=="Blue"])
